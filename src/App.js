abad3ff1f55f3f5f63132c0b84b641d9c0b97ee8
Vanshika Reddy <vanshureddy@gmail.com>
2022-03-12 15:30:57 -0600

    scrolling added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
-import soccer from './soccer.jpg';
+import "./App.css";
+import soccer from "./soccer.jpg";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <img src={soccer} className="App-logo" alt="Hi"/>
+      <img
+        src={soccer}
+        className="App-logo"
+        alt="Hi"
+        style={{ marginTop: `${scrollPosition * 1.9}px` }}
+      />
     </div>
   );
 }
